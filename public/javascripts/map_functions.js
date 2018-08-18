74061477fb4a71fa5771e07f98147f5357d89bb7
NiklasAsselmann <n_asse04@uni-muenster.de>
2018-08-18 11:26:31 +0200

    add Fachschaft

--- public/javascripts/map_functions.js
+++ public/javascripts/map_functions.js
@@ -17,24 +17,51 @@ function readURLFromTA() {
 *@desc add and load the read GeoJSON/URL on the map
 */
 function loadGeoJSON() {
-    try {
     var feat = readGeoJSONFromTA();
-    var gLayer = L.geoJson(feat);
-    gLayer.addTo(map);
-}
-    catch {
-        alert("Bitte korrektes GeoJSON eingeben")
-    }
-}
-function loadURL() {
+    var bild = feat.features[0].properties.img;
+    var iname = feat.features[0].properties.name;
+    var laylay = L.geoJson(feat);
+    var LATLON = coordinateMean(feat);
+    var maymay = L.marker([LATLON.LATmean, LATLON.LONmean]);
     try {
-        var feat = readURLFromTA();
-        var gLayer = L.geoJson(feat);
-        gLayer.addTo(map);
+        laylay.addTo(map);
+        maymay.addTo(map).bindPopup("<h5>"+iname+"<h5><img src="+bild+" width='200'><br>").openPopup();
+        drawnItems.addLayer(laylay);
+        drawnItems.addLayer(maymay);
+        $('#delete').prop('disabled', false);
+        $('#download').prop('disabled', false);    
     }
     catch {
-            alert("Bitte korrekte URL eingeben")
+        alert("Bidde korrektes JSON");
     }
+}
+
+function loadURL() {
+    var feat;
+    var url = document.getElementById('url-area').value;
+    fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        feat = json;
+        var bild = feat.features[0].properties.img;
+        var iname = feat.features[0].properties.name;
+        var laylay = L.geoJson(feat);
+        var LATLON = coordinateMean(feat);
+        var maymay = L.marker([LATLON.LATmean, LATLON.LONmean]);
+    try {    
+        laylay.addTo(map);
+        maymay.addTo(map).bindPopup("<h5>"+iname+"<h5><img src="+bild+" width='200'><br>").openPopup();
+        drawnItems.addLayer(laylay);
+        drawnItems.addLayer(maymay);
+        $('#delete').prop('disabled', false);
+        $('#download').prop('disabled', false); 
+    }
+    catch {
+        alert("Bidde korrekten Link");
+        }
+        }
+    )
+
 }
 
 /**
@@ -67,22 +94,23 @@ class databaseobject {
  * @desc makes an AJAX post request with the data to later store it in the database
  */
 function saveToDatabase() {
-    var textfield = document.getElementById('institutsname-area').value; 
-    var imageurl = document.getElementById('imageurl-area').value; 
-    //console.log(textfield);
-    
-
+    var textfield = document.getElementById('GeoJSONname').value;     
     if(textfield.length==0) {
-        alert("Bitte Name eingeben");
+        alert("Bitte Namen eingeben");
     }   else {
         var data = drawnItems.toGeoJSON();
-        var dbObject = new databaseobject(textfield, "",imageurl);
+        var dbObject = new databaseobject(textfield, "");
         dbObject.json = JSON.stringify(data);
-        alert("Object erfolgreich gespeichert!");
         $.ajax({
             type: 'POST',
             data: dbObject,
             url: "./start",
+            success: function(result){
+                $('#error').html("Objekt gespeichert");
+            },
+            error: function(xhr,status,error){
+                $('#error').html("Ups");
+            }
         });
     }
 }
